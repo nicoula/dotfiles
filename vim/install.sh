619d97e7392743838c9ebd1dfae153f3b87cfb34
Nicolas Mingault <nicoula@gmail.com>
2016-10-10 19:49:43 +0800

    [VIM] check if vim is installed; remove git clone comment

--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,14 @@
 
 echo "Vim"
 
+# test if vim is installed
+vim_exists=`which vim > /dev/null 2>&1 || echo no`
+
+if [[ $vim_exists == "no"  ]]; then
+        echo "Vim is not installed. Aborting."
+        exit 1
+fi
+
 # Install the vim configuration file
 echo "# .vimrc"
 if [ -e ~/.vimrc ]; then
@@ -15,21 +23,6 @@ mkdir -p ~/.vim/autoload
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install plugins for vim
-#echo "# delitMate"
-#git clone https://github.com/Raimondi/delimitMate ~/.vim/bundle/delimitMate
-#echo "# FuzzyFinder"
-#git clone https://github.com/vim-scripts/FuzzyFinder.git ~/.vim/bundle/FuzzyFinder
-#echo "# Supertab"
-#git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
-#echo "# vim-colorschemes"
-#git clone https://github.com/flazz/vim-colorschemes ~/.vim/bundle/vim-colorschemes
-#echo "# vim-fugitive"
-#git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
-#echo "# vim-l9"
-#git clone https://github.com/vim-scripts/L9 ~/.vim/bundle/vim-l9
-#echo "# vim-misc"
-#git clone URL: https://github.com/xolox/vim-misc ~/.vim/bundle/vim-misc
-
 echo "# Update submodule"
 git submodule update --init --recursive
 ln -s $PWD/bundle ~/.vim/bundle
@@ -38,3 +31,4 @@ ln -s ~/.vim/bundle/vim-colorschemes/colors ~/.vim/colors
 
 echo
 echo
+
