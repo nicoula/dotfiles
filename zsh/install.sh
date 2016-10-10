1397e6219745a3642fb744807e78b3ce247a793b
Nicolas Mingault <nicoula@gmail.com>
2016-10-10 20:28:09 +0800

    [ZSH] fix previous commit by commenting the left over from vim install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -27,14 +27,14 @@ ln -s $PWD/.zshrc ~/.zshrc
 
 
 # Install pathogen
-echo "# pathogen"
-mkdir -p ~/.vim/autoload
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Install plugins for vim
-echo "# Update submodule"
-git submodule update --init --recursive
-ln -s $PWD/bundle ~/.vim/bundle
-
-ln -s ~/.vim/bundle/vim-colorschemes/colors ~/.vim/colors
+#echo "# pathogen"
+#mkdir -p ~/.vim/autoload
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#
+## Install plugins for vim
+#echo "# Update submodule"
+#git submodule update --init --recursive
+#ln -s $PWD/bundle ~/.vim/bundle
+#
+#ln -s ~/.vim/bundle/vim-colorschemes/colors ~/.vim/colors
 
