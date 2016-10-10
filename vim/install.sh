d207920cfe5deade9becc973eef3a69db3403325
Nicolas Mingault <nicolas.mingault@hp.com>
2016-10-10 14:56:58 +0800

    [VIM] fix installation bug

--- vim/install.sh
+++ vim/install.sh
@@ -2,28 +2,13 @@
 
 echo "Installing vim's files"
 
-## Install the vim configuration file
-#if [ -e ~/.vimrc ]; then
-#        rm ~/.vimrc
-#fi
-#ln -s $(PWD)/.vimrc ~/.vimrc
-#
-## Install pathogen
-#mkdir -p ~/.vim/autoload ~/.vim/bundle
-#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-
-
-
-
 # Install the vim configuration file
-VIMDIR=~/test
-VIMRC_LOCATION=$(VIMDIR)/.vimrc
-if [ -e $(VIMRC_LOCATION) ]; then
-        rm $(VIMRC_LOCATION)
+if [ -e ~/.vimrc ]; then
+        rm ~/.vimrc
 fi
-ln -s $(PWD)/.vimrc $(VIMRC_LOCATION)
+ln -s $PWD/.vimrc ~/.vimrc
 
 # Install pathogen
-mkdir -p ~/$(VIMDIR)/.vim/autoload ~/$(VIMDIR)/.vim/bundle
-curl -LSso ~/$(VIMDIR)/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
